67f62ab96610a7a6617bd747807c7377229c901d
Sara Muschkin <sunshinesara55@MacBook-Pro-2.local>
2018-07-26 18:19:27 -0400

    ugh

--- routing/apiRoutes.js
+++ routing/apiRoutes.js
@@ -9,12 +9,29 @@ module.exports = function (app) {
 
     app.post("/api/friends", function (req, res) {
         var match = [50, 10];
+        var smallestDifference = 1000;
         for (var i = 0; i < friends.length; i++) {
+            
             var enemy = 0;
-            for (var i = 0; i < req.body.scores.length; i++) {
-                enemy += Math.abs(parseInt(friends[i].scores[i]) - parseInt(req.body.scores[i]));
+            for (var j = 0; j < req.body.scores.length; j++) {
+              (enemy += Math.abs(parseInt(friends[i].scores[i]) 
+              - parseInt(req.body.scores[i])));
             }
-        }
+            if(enemy <= smallestDifference) {
+                enemy = smallestDifference
+            }
+            // res.body.scores
+                
+            }
+            return smallestDifference;
+
+            friends.name[0].push(req.body.modal);
+            friends.name[1].push(req.body);
+            friends.name[2].push(req.body);
+            friends.name[3].push(req.body);
+
+
+
         
     });
 
